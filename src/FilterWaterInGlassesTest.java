2c69586ae8b0ece21b1d6eb023c71fc7a73c571f
mayuresh-sawant <44738691+mayuresh-sawant@users.noreply.github.com>
2019-03-12 18:27:18 +1100

    Update FilterWaterInGlassesTest.java
    Added a new tests

--- src/FilterWaterInGlassesTest.java
+++ src/FilterWaterInGlassesTest.java
@@ -15,6 +15,13 @@ public class FilterWaterInGlassesTest {
 		WaterOverflow waterOverFlow = new WaterOverflow();
 		int result = waterOverFlow.fillWaterInGlasses(1, 10);
 		assertEquals(4, result);
+		
+		
+		int resultWithZeroCapacity = waterOverFlow.fillWaterInGlasses(0, 10);
+		assertEquals(0, resultWithZeroCapacity);
+		
+		int resultWithZeroWater = waterOverFlow.fillWaterInGlasses(2.5, 0);
+		assertEquals(0, resultWithZeroWater);
 	}
 
 }
